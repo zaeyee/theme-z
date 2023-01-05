dce404dd0bfb0da1b839637c7c736fbb835013ad
zaeyee <zaeyee@163.com>
2023-01-06 00:10:08 +0800

    pending: Navbar

--- src/components/ZNavbar.vue
+++ src/components/ZNavbar.vue
@@ -9,32 +9,27 @@ const toggleDark = useToggle(isDark)
     <a href="/" flex-none w-10 h-10>
       <ZImage :image="theme.logo" />
     </a>
-    <nav flex="~" justify-center items-center>
-      <a
-        v-for="(item, index) in theme.nav"
-        :key="index"
-        px-2.5
-        color="black/50 hover:black/70"
-        transition-colors
-        :href="item.link"
-      >
-        {{ item.text }}
-      </a>
-      <a
-        v-for="(item, index) in theme.socialLinks"
-        :key="index"
-        :href="item.link"
-        :class="{ 'b-l': index === 0 }"
-        color="black/50 hover:black"
-        px-2.5
-        transition-colors
-      >
-        <div text-lg :class="item.icon"></div>
-      </a>
-      <div b-l px-2.5 text-lg cursor-pointer transition-colors color="black/50 hover:black" @click="toggleDark()">
+    <div flex="~" items-center>
+      <nav>
+        <a v-for="(item, index) in theme.nav" :key="index" :href="item.link" class="nav-item">
+          {{ item.text }}
+        </a>
+      </nav>
+      <nav flex="~" items-center b-l>
+        <a v-for="(item, index) in theme.socialLinks" :key="index" :href="item.link" class="nav-item">
+          <div text-lg :class="item.icon"></div>
+        </a>
+      </nav>
+      <div b-l text-lg class="nav-item" @click="toggleDark()">
         <div v-show="!isDark" i-lucide:sun></div>
         <div v-show="isDark" i-lucide:moon></div>
       </div>
-    </nav>
+    </div>
   </header>
 </template>
+
+<style scoped>
+.nav-item {
+  @apply px-2.5 color-black/50 hover:color-black cursor-pointer transition-colors duration-300;
+}
+</style>
